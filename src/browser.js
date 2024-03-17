3b3da9c14f79d642a7d535bc465a712b4c0fe32e
SparrowXD-beuh <tajneyashraj15@gmail.com>
2024-03-17 14:09:27 +0530

    dockerize the project

--- src/browser.js
+++ src/browser.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer-extra");
-const Chromium = require("chrome-aws-lambda");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 let browser;
@@ -7,7 +6,6 @@ const getBrowser = async () => {
     if (!browser) {
         puppeteer.use(StealthPlugin());
         browser = await puppeteer.launch({
-            executablePath: await Chromium.executablePath,
             headless: true,
         });
     }
